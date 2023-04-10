0e9c026472868f1ec6bf7c458d0ebdd08f7d9d2f
yubin <ubineda@gmail.com>
2023-04-10 16:49:57 +0900

    type: feat
    body:

--- src/main/java/sm/school/domain/contest/Contest.java
+++ src/main/java/sm/school/domain/contest/Contest.java
@@ -1,4 +1,23 @@
 package sm.school.domain.contest;
 
+import lombok.Getter;
+import sm.school.domain.member.Member;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class Contest {
+    @Id
+    @GeneratedValue
+    @Column(name = "contest_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    private String con_name;//컨테스트 이름
+    private String con_inf;//컨테스트 정보
+    private String con_picture;//대문사진
+
+
 }
