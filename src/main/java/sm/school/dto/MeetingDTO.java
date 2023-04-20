8beff8eb239b5456830d8652b993c5e3de54f7a6
김재우 <ksg3477@gmail.com>
2023-04-20 15:57:14 +0900

    type: feat
    body: MeetingDTO, MeetingRoomDTO DTO 생성 및
    DTO -> Entity 변환 메서드 추가

---
+++ src/main/java/sm/school/dto/MeetingDTO.java
@@ -0,0 +1,55 @@
+package sm.school.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import sm.school.domain.meeting.Meeting;
+import sm.school.domain.member.Member;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MeetingDTO {
+
+    private Long id;
+
+    @NotEmpty//해당 필드의 값이 null이 아니고, 비어있지 않아야 함
+    private String school; //학교
+
+    @NotEmpty
+    private String major; // 전공
+
+    @NotEmpty
+    private String region; //지역
+
+    @NotEmpty
+    private int count; //미팅인원
+
+    @NotEmpty
+    private Member member;//미팅 생성 회원
+
+    @Builder
+    public MeetingDTO(Long id, String school, String major, String region, int count, Member member) {
+        this.id = id;
+        this.school = school;
+        this.major = major;
+        this.region = region;
+        this.count = count;
+        this.member = member;
+    }
+
+    public Meeting toMeetingEntity() {
+        return Meeting.builder()
+                .id(id)
+                .school(school)
+                .major(major)
+                .region(region)
+                .count(count)
+                .member(member)
+                .build();
+    }
+}
