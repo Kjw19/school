07f4f160ed89f22cf81e62bdf71c7ecdce865304
yubin <ubineda@gmail.com>
2023-04-23 17:33:27 +0900

    type: refactor
    body:Contest,ContestDTO,ContestRepository,ContestService 객체 수정

--- src/main/java/sm/school/domain/contest/ContestDTO.java
+++ src/main/java/sm/school/domain/contest/ContestDTO.java
@@ -5,48 +5,45 @@ import sm.school.domain.enumType.ContestType;
 import sm.school.domain.enumType.LocationType;
 import sm.school.domain.member.Member;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ContestDTO {
-    private Long id;
-    private Member member;
+
+    @NotEmpty(message = "대회 이름은 필수")
+    //private Member member;
     private String conName;
     private String conInf;
     private ContestType contestType;
     private LocationType locationType;
     private String location;
     private String conPicture;
-    private Date date;
-    private int regType;
+    private Integer regType;
     @Builder
-    public ContestDTO(Long id, Member member, String conName, String conInf, ContestType contestType
-            , LocationType locationType, String location, String conPicture, Date date, int regType) {
-        this.id = id;
-        this.member = member;
+    public ContestDTO(/*Member member*/ String conName, String conInf, ContestType contestType
+            , LocationType locationType, String location, String conPicture, Integer regType) {
+        //this.member = member;
         this.conName = conName;
         this.conInf = conInf;
         this.contestType = contestType;
         this.locationType = locationType;
         this.location = location;
         this.conPicture = conPicture;
-        this.date = date;
         this.regType = regType;
     }
 
-    public Contest toEntity(){
+    public Contest toContestEntity(){
         return Contest.builder()
-                .id(this.id)
-                .member(this.member)
+                //.member(this.member)
                 .conName(this.conName)
                 .conInf(this.conInf)
                 .contestType(this.contestType)
                 .locationType(this.locationType)
                 .location(this.location)
                 .conPicture(this.conPicture)
-                .date(this.date)
                 .regType(this.regType)
                 .build();
     }
