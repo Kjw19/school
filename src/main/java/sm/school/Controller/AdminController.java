668bbe3ec3e790e17a8abde05ead3cbad12b2336
김재우 <ksg3477@gmail.com>
2023-05-11 16:04:30 +0900

    type: feat
    body:
    1. AdminController에 미팅 방 목록을 볼 수 있는 리스트 추가
    2. adminPage 에 미팅 목록으로 이동 할 수 있는 링크 추가
    3. manageMeeting 에서 삭제 및 상세정보 들어갈 수 있는 링크가 있는 폼 추가

--- src/main/java/sm/school/Controller/AdminController.java
+++ src/main/java/sm/school/Controller/AdminController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sm.school.Service.MeetingProposerService;
+import sm.school.Service.MeetingService;
 import sm.school.Service.MemberService;
 import sm.school.domain.enumType.MemRole;
 import sm.school.dto.MemberDTO;
@@ -21,8 +23,12 @@ import java.util.List;
 public class AdminController {
 
     private final MemberService memberService;
+    private final MeetingService meetingService;
+    private final MeetingProposerService meetingProposerService;
+
 
     @GetMapping
+
     public String admin(Authentication authentication) {
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
@@ -71,5 +77,41 @@ public class AdminController {
         memberService.changeRole(id);
 
         return "redirect:/admin/member";
+    }
+
+    @GetMapping("/meeting")
+    public String manageMeeting(Model model, Authentication authentication) {
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        boolean isAdmin = authorities.stream()
+                .anyMatch(grantedAuthority -> MemRole.ADMIN.getValue().equals(grantedAuthority.getAuthority()));
+
+        if (!isAdmin) {
+            return "accessBlock";
+        }
+        model.addAttribute("List", meetingService.findMeeting());
+
+        return "/admin/manageMeeting";
+    }
+
+    @RequestMapping("deleteMeeting")
+    public String deleteMeeting(@RequestParam("id") Long id, Authentication authentication) {
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        boolean isAdmin = authorities.stream()
+                .anyMatch(grantedAuthority -> MemRole.ADMIN.getValue().equals(grantedAuthority.getAuthority()));
+
+        if (!isAdmin) {
+            return "accessBlock";
+        }
+
+        Boolean deleteMeeting = meetingService.DeleteMeeting(id);
+        if (deleteMeeting) {
+            return "redirect:/admin/meeting";
+        } else {
+            return "redirect:/errorPage";
+        }
     }
 }
