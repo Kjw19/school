a78eeb056b78c676f1130133e3f0bc7fb1a65a35
김재우 <ksg3477@gmail.com>
2023-05-11 14:41:52 +0900

    type: feat
    body:
    1. AdminController에 회원 전체 정보를 볼수있는 로직 추가 및 회원권한변경 로직 추거
    2. manageMember을 통해 전체 회원 정보 조회 및 회원 권한 수정 하는 폼 추가
    3. adminPage에 manageMember로 이동 할 수 있도록 추가

--- src/main/java/sm/school/Controller/AdminController.java
+++ src/main/java/sm/school/Controller/AdminController.java
@@ -1,17 +1,75 @@
 package sm.school.Controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sm.school.Service.MemberService;
+import sm.school.domain.enumType.MemRole;
+import sm.school.dto.MemberDTO;
+
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
+    private final MemberService memberService;
 
     @GetMapping
-    public String admin() {
+    public String admin(Authentication authentication) {
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        boolean isAdmin = authorities.stream()
+                .anyMatch(grantedAuthority -> MemRole.ADMIN.getValue().equals(grantedAuthority.getAuthority()));
+
+        if (!isAdmin) {
+            return "accessBlock";
+        }
 
         return "admin/adminPage";
+    }
+
+    @GetMapping("/member")
+    public String manageMember(Model model, Authentication authentication) {
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        boolean isAdmin = authorities.stream()
+                .anyMatch(grantedAuthority -> MemRole.ADMIN.getValue().equals(grantedAuthority.getAuthority()));
+
+        if (!isAdmin) {
+            return "accessBlock";
+        }
+
+        List<MemberDTO> memberDTOList = memberService.memberList();
+
+        model.addAttribute("List", memberDTOList);
+
+        return "admin/manageMember";
+    }
+
+    @RequestMapping("changeRole")
+    public String changeRole(@RequestParam("id") Long id, Authentication authentication) {
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        boolean isAdmin = authorities.stream()
+                .anyMatch(grantedAuthority -> MemRole.ADMIN.getValue().equals(grantedAuthority.getAuthority()));
+
+        if (!isAdmin) {
+            return "accessBlock";
+        }
+
+        memberService.changeRole(id);
+
+        return "redirect:/admin/member";
     }
 }
